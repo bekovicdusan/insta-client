93f524b501c6228a1df208dfa2da4dc2daf04bb5
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-07 23:51:17 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,55 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue'),
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/RegisterView.vue'),
+    meta: {
+      requiresAuth: false
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') === null) {
+      next({
+        path: '/login',
+        name: 'login'
+      })
+    } else {
+      console.log('Logged In')
+      next()
+    }
+  } else {
+    console.log('doesnt require auth')
+    next()
+  }
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Pinkie Pink</h3>
+    <div class="form-group">
+      <input type="text" id="email" v-model="email">
+      <input type="text" id="password" v-model="password">
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const email = ref('')
+    const password = ref('')
+
+    return {
+      email,
+      password
+    }
+  }
+}
+</script>
