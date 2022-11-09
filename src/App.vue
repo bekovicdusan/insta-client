7630f1d7de15ee74a13fcb889df6f14286e68d94
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-09 15:38:39 +0100

    implemented font

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
   <router-view/>
 </template>
+
+<style>
+@font-face {
+  font-family: "Nunito";
+  src: local("Nunito"),
+   url('./fonts/Nunito-Medium.ttf') format("truetype");
+}
+
+body {
+  min-height: 100vh;
+  color: #303030;
+}
+
+body, button {
+  font-family: 'Nunito';
+}
+</style>
