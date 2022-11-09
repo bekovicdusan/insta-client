dff0241337f0b2dd05bbe961c1e4a0308e07f19f
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-09 19:32:03 +0100

    restructured styles

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,6 @@
   <router-view/>
 </template>
 
-<style>
-@font-face {
-  font-family: "Nunito";
-  src: local("Nunito"),
-   url('./fonts/Nunito-Medium.ttf') format("truetype");
-}
-
-body {
-  min-height: 100vh;
-  color: #303030;
-}
-
-body, button {
-  font-family: 'Nunito';
-}
+<style lang="scss">
+@import './styles/main.scss';
 </style>
