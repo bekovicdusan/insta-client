9d5bfe3b06da1dfad759f8a2c88093c22a1a059f
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-10 18:14:13 +0100

    imported font awesome icons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHouse, faMagnifyingGlass, faImages, faBell, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(faHouse, faMagnifyingGlass, faImages, faBell, faUser)
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
