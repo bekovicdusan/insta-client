dbc64359eefe1329c186d3aad5debaa38d8e9102
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-10 15:18:33 +0100

    moved login and logout to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import { createStore } from 'vuex'
+import router from '@/router'
 
 export default createStore({
   state: {
-    api_url: 'http://localhost:3000/'
+    api_url: 'http://localhost:3000/',
+    isAuthenticated: false
   },
   getters: {
   },
   mutations: {
+    login (state, token) {
+      state.isAuthenticated = true
+      localStorage.setItem('jwt', token)
+      router.push('/')
+    },
+    logout (state) {
+      state.isAuthenticated = false
+      localStorage.removeItem('jwt')
+      router.push('/login')
+    }
   },
   actions: {
-  },
-  modules: {
   }
 })
