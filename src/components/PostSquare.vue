e84b66ebafb304fc9f7d3b80fec1b2bdbda4cfb4
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-12 21:58:32 +0100

    added individual profile posts component

---
+++ src/components/PostSquare.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="post-frame" :style="`background-image: url(${post.image})`">
+    <!-- <img :src="post.image" :alt="post.caption"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
