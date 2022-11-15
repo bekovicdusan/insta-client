3d4e00d2deff1e0d0ba7bf2df1422568a549808b
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-16 00:09:42 +0100

    updated postsquare component

--- src/components/PostSquare.vue
+++ src/components/PostSquare.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="post-frame" :style="`background-image: url(${post.image})`">
-    <!-- <img :src="post.image" :alt="post.caption"> -->
+  <div class="post-frame"
+    :style="`background-image: url(${post.image})`"
+    @mouseover="showIcons = !showIcons"
+    @mouseleave="showIcons = !showIcons"
+    >
+    <div class="icon-container" v-if="showIcons">
+      <div class="icon">
+        <font-awesome-icon icon="fa-heart"/>
+        <span>{{ likes }}</span>
+      </div>
+      <div class="icon">
+        <font-awesome-icon icon="fa-comment"/>
+        <span>{{ comments }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   props: {
     post: {
       type: Object,
       required: true
+    }
+  },
+  setup () {
+    const likes = ref(0)
+    const comments = ref(0)
+    const showIcons = ref(false)
+
+    return {
+      likes,
+      comments,
+      showIcons
     }
   }
 }
