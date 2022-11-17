815f1d28363e9920da857138e3ee02da632f29f1
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-17 02:23:02 +0100

    added profile menu and refactored header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,7 +1,44 @@
 <template>
-  <header class="header">
-    <router-link to="/">
-      <h1 class="headline">PinkyPink</h1>
-    </router-link>
-  </header>
+  <div class="header-container">
+    <header class="header">
+      <div class="search-div">
+        <font-awesome-icon class="icon" icon="fa-magnifying-glass"/>
+        <input type="text" placeholder="Search">
+      </div>
+      <router-link to="/">
+        <h1 class="headline">PinkyPink</h1>
+      </router-link>
+      <img :src="profileImg" alt="profile img" class="profile-img" @click="showProfileMenu">
+      <profile-menu v-if="showMenu" />
+      <div class="modal-overlay profile-menu" v-if="showMenu" @click="showProfileMenu"></div>
+    </header>
+  </div>
 </template>
+
+<script>
+import { computed } from 'vue'
+
+import ProfileMenu from '@/components/ProfileMenu'
+import store from '@/store'
+
+export default {
+  components: {
+    ProfileMenu
+  },
+  setup () {
+    const profileImg = computed(() => store.state.currentUser.profileImg)
+    const showMenu = computed(() => store.state.showProfileMenu)
+
+    const showProfileMenu = () => {
+      store.commit('showProfileMenu')
+    }
+
+    return {
+      profileImg,
+      showMenu,
+
+      showProfileMenu
+    }
+  }
+}
+</script>
---
+++ src/components/ProfileMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="menu-container">
+    <router-link :to="`/profile/${username}`" @click="showProfileMenu" class="nav-item">
+      <li class="menu-item profile-item">
+        <font-awesome-icon class="icon" icon="fa-user"/>
+        <span>Profile</span>
+      </li>
+    </router-link>
+    <router-link :to="`/profile/${username}/saved`" @click="showProfileMenu" class="nav-item">
+      <li class="menu-item">
+        <font-awesome-icon class="icon" icon="fa-bookmark"/>
+        <span>Saved</span>
+      </li>
+    </router-link>
+    <router-link to="/account/edit" @click="showProfileMenu">
+      <li class="menu-item">
+        <font-awesome-icon class="icon" icon="fa-gear"/>
+        <span>Settings</span>
+      </li>
+    </router-link>
+    <li class="menu-item" @click="showProfileMenu">
+      <font-awesome-icon class="icon" icon="fa-repeat"/>
+      <span>Switch accounts</span>
+    </li>
+    <li class="menu-item logout" @click="logout">Log out</li>
+  </ul>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+import store from '@/store'
+
+export default {
+  setup () {
+    const username = computed(() => store.state.currentUser.username)
+
+    const logout = () => {
+      store.commit('logout')
+      showProfileMenu()
+    }
+
+    const showProfileMenu = () => {
+      store.commit('showProfileMenu')
+    }
+
+    return {
+      username,
+
+      logout,
+      showProfileMenu
+    }
+  }
+}
+</script>
