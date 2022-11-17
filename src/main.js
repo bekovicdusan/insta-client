2af7a979002bd8d04a9bc56cf4881e1ecce54f69
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-17 02:23:20 +0100

    added new icons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHouse, faMagnifyingGlass, faImages, faBell, faUser, faCamera, faXmark, faUpload, faHeart, faComment, faGear } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faMagnifyingGlass, faImages, faBell, faUser, faCamera, faXmark, faUpload, faHeart, faComment, faGear, faBookmark, faUserTag, faImage, faCompass, faRepeat } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHouse, faMagnifyingGlass, faImages, faBell, faUser, faCamera, faXmark, faUpload, faHeart, faComment, faGear)
+library.add(faHouse, faMagnifyingGlass, faImages, faBell, faUser, faCamera, faXmark, faUpload, faHeart, faComment, faGear, faBookmark, faUserTag, faImage, faCompass, faRepeat)
 createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
