acb6af0a144e664edff269ba79c18bf0280bbe02
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-17 02:24:53 +0100

    changed explore icon in footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="icon-container">
       <button class="primary-button icon" @click="goTo('/')"><font-awesome-icon class="icon" icon="fa-house"/></button>
-      <button class="primary-button icon"><font-awesome-icon class="icon" icon="fa-magnifying-glass"/></button>
+      <button class="primary-button icon"><font-awesome-icon class="icon" icon="fa-compass"/></button>
       <button class="primary-button icon" @click="goTo('/newpost')"><font-awesome-icon class="icon" icon="fa-images"/></button>
       <button class="primary-button icon"><font-awesome-icon class="icon" icon="fa-bell"/></button>
       <button class="primary-button icon" @click="goTo('Profile', storeVar.state.currentUser.username)"><font-awesome-icon class="icon" icon="fa-user"/></button>
