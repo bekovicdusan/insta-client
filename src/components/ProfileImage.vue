7c973a16f45a61b8778f17ab2d423d7fae9894d5
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-18 12:04:19 +0100

    moved profile image to component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="image" alt="profile img" class="profile-img" :style="{ width: size + 'px', height: size + 'px' }">
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
