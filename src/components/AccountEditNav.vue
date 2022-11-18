5dc3d8f782b19a798318d7c70baebaefdade40ad
bekovicdusan <bekovicdusan94@gmail.com>
2022-11-18 12:09:20 +0100

    added footer and account navigation components

---
+++ src/components/AccountEditNav.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="account-edit-nav-container">
+    <ul class="nav-collection">
+      <router-link to="/account/edit">
+        <li class="nav-item">Edit profile</li>
+      </router-link>
+      <router-link to="/account/change-password">
+        <li class="nav-item">Change password</li>
+      </router-link>
+      <router-link to="/account/apps-and-websites">
+        <li class="nav-item">Apps and websites</li>
+      </router-link>
+      <router-link to="/account/email-notifications">
+        <li class="nav-item">Email notifications</li>
+      </router-link>
+      <router-link to="/account/push-notifications">
+        <li class="nav-item">Push notifications</li>
+      </router-link>
+      <router-link to="/account/manage-contacts">
+        <li class="nav-item">Manage contacts</li>
+      </router-link>
+      <router-link to="/account/privacy-and-security">
+        <li class="nav-item">Privacy and security</li>
+      </router-link>
+      <router-link to="/account/ads">
+        <li class="nav-item">Ads</li>
+      </router-link>
+      <router-link to="/account/supervision">
+        <li class="nav-item">Supervision</li>
+      </router-link>
+      <router-link to="/account/login-activity">
+        <li class="nav-item">Login activity</li>
+      </router-link>
+      <router-link to="/account/emails-from-pinkypink">
+        <li class="nav-item">Emails from PinkyPink</li>
+      </router-link>
+      <router-link to="/account/help">
+        <li class="nav-item">Help</li>
+      </router-link>
+      <router-link to="/account/digital-collectibles">
+        <li class="nav-item">Digital collectibles</li>
+      </router-link>
+      <router-link to="/">
+        <li class="nav-item last">Switch to professional account</li>
+      </router-link>
+    </ul>
+    <div class="infinity">
+      <div class="logo">
+        <font-awesome-icon class="icon" icon="fa-infinity"/>
+        <span>Infinity</span>
+      </div>
+      <h4 class="headline-secondary">Accounts Center</h4>
+      <p class="copyright">
+        Control settings for connected experiences across PinkyPink, the PinkyPink app and chat system, including story and post sharing and logging in
+      </p>
+    </div>
+  </div>
+</template>
---
+++ src/components/AccountFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer-container">
+    <ul class="links">
+      <li class="link">Meta</li>
+      <li class="link">About</li>
+      <li class="link">Blog</li>
+      <li class="link">Jobs</li>
+      <li class="link">Help</li>
+      <li class="link">API</li>
+      <li class="link">Privacy</li>
+      <li class="link">Terms</li>
+      <li class="link">Top Accounts</li>
+      <li class="link">Hashtags</li>
+      <li class="link">Locations</li>
+      <li class="link">Instagram Lite</li>
+      <li class="link">Contact Uploading & Non-Users</li>
+    </ul>
+    <div class="footer-lower">
+      <span class="language">English</span>
+      <span>© 2022 PinkyPink</span>
+    </div>
+  </div>
+</template>
